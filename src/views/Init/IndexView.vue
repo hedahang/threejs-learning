442f60bae9ac734e62d192e782ccf0744421f563
munachar <hdh_angular@163.com>
2024-08-08 10:57:57 +0800

    feat: initView文档

--- src/views/Init/IndexView.vue
+++ src/views/Init/IndexView.vue
@@ -22,26 +22,33 @@ onMounted(() => {
   scene.background = new Color("black");
 
   // 创建相机
-  const camera = new PerspectiveCamera(35, containerWidth / containerHeight, 0.1, 100);
-  camera.position.set(0, 0, 10);
+  const camera = new PerspectiveCamera(45, containerWidth / containerHeight, 1, 100);
+  // 设置相机位置，将其朝向我们移动
+  camera.position.set(0, 0, 5);
+  // camera.position.set(-2, -1, 5);
 
   // 创建渲染器
   const renderer = new WebGLRenderer();
   // 设置渲染器的尺寸
   renderer.setSize(containerWidth, containerHeight);
+  // renderer.setSize(containerWidth/2, containerHeight/2);
 
   // 将渲染器的输出添加到容器中
   container?.appendChild(renderer.domElement);
 
   // 创建立方体
   const geometry = new BoxGeometry(1, 1, 1);
-  // 创建材质
-  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+  // const geometry = new THREE.CapsuleGeometry(1, 1, 4,8);
+  // 创建材质 https://threejs.org/docs/#api/zh/materials/MeshPhongMaterial
+  // const material = new MeshBasicMaterial({ color: 0x00ff00});
+  // 创建法线网格材质
+  const material = new THREE.MeshNormalMaterial();
   // 创建网格
   const cube = new THREE.Mesh(geometry, material);
   // 添加立方体到场景
   scene.add(cube);
 
+  // 动画
   function animate() {
     requestAnimationFrame(animate);
 
